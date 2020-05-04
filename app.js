5bc4982999420fef3eb2f8082d82c2ffeb940621
Diego De Quintal <dadequintal.17@est.ucab.edu.ve>
2020-05-04 00:21:32 -0400

    Commit

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const compression = require('compression');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const compression = require("compression");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(compression());
+
+app.get("/", (req, res) => {
+  console.log("Epalee");
+  res.json("OK");
+});
 
 module.exports = app;
