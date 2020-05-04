a8f36d4da374bf249b82941dd305f70923e28be0
Diego De Quintal <dadequintal.17@est.ucab.edu.ve>
2020-05-04 01:04:54 -0400

    commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const compression = require("compression");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
@@ -14,3 +15,20 @@ app.get("/", (req, res) => {
 });
 
 module.exports = app;
+
+const enviar = async () => {
+  try {
+    const res = await axios({
+      method: "POST",
+      url: "http://localhost:3000/bodenchufe/api/user/recibirPago",
+      data: {
+        hola: "diego",
+      },
+    });
+    console.log(res);
+  } catch (err) {
+    console.log("err");
+  }
+};
+
+enviar();
