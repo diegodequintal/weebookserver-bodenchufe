71755e48325f64f7a9edb02628227b053707b090
Diego De Quintal <dadequintal.17@est.ucab.edu.ve>
2020-05-04 00:07:36 -0400

    Server actualizado

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const app = require("./app");
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
 
-const server = app.listen(4000, () => {
-  console.log(`App running on port 4000...`);
+
+const port = process.env.PORT;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
 });
